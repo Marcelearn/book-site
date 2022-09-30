0b70bc2d9fcbac1dc682e37bb92af0dfc764e4e6
Marcelearn <jaworskimarcel@gmail.com>
2022-09-30 11:28:02 +0200

    final test

--- js/model.js
+++ js/model.js
@@ -1,6 +1,24 @@
+const techBookUrl = 'https://edwardtanguay.netlify.app/share/techBooks.json';
+
 export const books = [
-    {title: "Psychology 101",
-    author: "George Anton"},
-    {title: "1984",
-    author: "George Orwell"}
-]
+    {
+        title: "Psychology 101",
+        author: "George Anton"
+    },
+    {
+        title: "1984",
+        author: "George Orwell"
+    }
+];
+
+export const getTechBooks = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            (async () => {
+                const response =  await fetch(techBooksUrl);
+                const techBooks = await response.json();
+                resolve(techBooks);
+            })();
+        },3000);   
+    });
+};  
