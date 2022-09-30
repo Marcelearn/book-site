543a777bb856fcd816b20c8a2c37197487e6ec66
Marcelearn <jaworskimarcel@gmail.com>
2022-09-30 10:25:49 +0200

    create book module

---
+++ js/main.js
@@ -0,0 +1,18 @@
+import { books } from "./model";
+const contentElem = document.querySelector(".content");
+
+
+contentElem.innerHTML = `
+    <h1>Book Site</h1>
+    <h2>There are ${books.length} books:</h2>
+
+    <div class = "books">
+        ${books.map(book => {
+            return `
+            <div class="book">
+            <div class = "title">${book.title}</div>
+            <div class = "description">${book.author}</div>
+            </div>`
+        }).join('')}
+    </div>
+`
---
+++ js/model.js
@@ -0,0 +1,6 @@
+export const books = [
+    {title: "Psychology 101",
+    author: "George Anton"},
+    {title: "1984",
+    author: "George Orwell"}
+]
